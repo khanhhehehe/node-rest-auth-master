f7d14d726a9715033cbef15fe53417aef0e3a2e5
khanhhehehe <117183565+khanhhehehe@users.noreply.github.com>
2023-04-07 17:50:20 +0700

    showlistbook

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,15 @@
 
 var passport = require('passport');
 var config = require('../config/database');
-require('../config/passport')(passport);
+require('../config/Passport')(passport);
 var express = require('express');
 var jwt = require('jsonwebtoken');
 var router = express.Router();
 var User = require("../models/user");
 var Book = require("../models/book");
-
 const bodyParser = require("body-parser");
+const request = require('request');
+
 
 // // parse requests of content-type - application/json
 router.use(bodyParser.json());
@@ -19,13 +20,16 @@ router.use(parser);
 router.get('/', (req, res) => {
     res.render('dangky')
 })
-router.get('/signin',(req,res)=>{
+router.get('/signin', (req, res) => {
     res.render('dangnhap')
 })
 router.post('/signup', async function (req, res) {
 
     if (!req.body.username || !req.body.password) {
-        res.json({ success: false, msg: 'Please pass username and password.' });
+        // res.json({ success: false, msg: 'Please pass username and password.' });
+        res.render('dangky', { warning: 'Please pass username and password.' })
+    } else if (req.body.password !== req.body.password2) {
+        res.render('dangky', { warning: 'Password is not same.' })
     } else {
         var newUser = new User({
             username: req.body.username,
@@ -33,12 +37,12 @@ router.post('/signup', async function (req, res) {
         });
         // save the user
         await newUser.save();
-        
+        res.redirect('/api/signin')
         // res.json({ success: true, msg: 'Successful created new user.' });
     }
 });
 
- 
+
 router.post('/signin', async function (req, res) {
     let user = await User.findOne({ username: req.body.username });
     if (!user) {
@@ -50,7 +54,14 @@ router.post('/signin', async function (req, res) {
                 // if user is found and password is right create a token
                 var token = jwt.sign(user.toJSON(), config.secret);
                 // return the information including token as JSON
-                res.json({ success: true, token: 'JWT ' + token });
+                // res.json({ success: true, token: 'JWT ' + token });
+
+                request.get('http://localhost:3000/api/book', {
+                    headers: { 'Authorization': 'JWT ' + token }
+                }, function (error, response, body) {
+                    res.send(body);
+                });
+
                 // res.redirect('/api/book')
             } else {
                 res.status(401).send({ success: false, msg: 'Authentication failed. Wrong password.' });
@@ -60,44 +71,46 @@ router.post('/signin', async function (req, res) {
 });
 
 
-// router.post('/book', passport.authenticate('jwt', { session: false }), function (req, res) {
-//     var token = getToken(req.headers);
-//     if (token) {
-//         console.log(req.body);
-//         var newBook = new Book({
-//             isbn: req.body.isbn,
-//             title: req.body.title,
-//             author: req.body.author,
-//             publisher: req.body.publisher
-//         });
+router.post('/book', passport.authenticate('jwt', { session: false }), function (req, res) {
+    var token = getToken(req.headers);
+    console.log('co: nhe');
+    if (token) {
+        console.log(req.body);
+        var newBook = new Book({
+            isbn: req.body.isbn,
+            title: req.body.title,
+            author: req.body.author,
+            publisher: req.body.publisher
+        });
 
-//         newBook.save(function (err) {
-//             if (err) {
-//                 return res.json({ success: false, msg: 'Save book failed.' });
-//             }
-//             res.json({ success: true, msg: 'Successful created new book.' });
-//         });
-//     } else {
-//         return res.status(403).send({ success: false, msg: 'Unauthorized.' });
-//     }
-// });
+        newBook.save(function (err) {
+            if (err) {
+                return res.json({ success: false, msg: 'Save book failed.' });
+            }
+            // res.json({ success: true, msg: 'Successful created new book.' });
+            res.redirect('/api/book')
+        });
+    } else {
+        return res.status(403).send({ success: false, msg: 'Unauthorized.' });
+    }
+});
 
 
 router.get('/book', passport.authenticate('jwt', { session: false }), async function (req, res) {
     var token = getToken(req.headers);
-    console.log(" co: "+token);
+    console.log('co token: ' + token);
     if (token) {
-        let books = await Book.find();
+        let books = await Book.find().lean();
 
-        res.json(books);
+        // res.json(books);
+        return res.render('listbook', { databooks: books })
     } else {
         return res.status(403).send({ success: false, msg: 'Unauthorized.' });
     }
 });
 
 getToken = function (headers) {
-    console.log(headers);
-    console.log(headers.authorization);
+    console.log('ok nhe');
     if (headers && headers.authorization) {
         var parted = headers.authorization.split(' ');
         if (parted.length === 2) {
